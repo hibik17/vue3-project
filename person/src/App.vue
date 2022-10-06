18876f7854bbfbcaa0a74fe785f7515effc06598
hibik17 <hbtatsu@gmail.com>
2022-10-06 22:30:49 +0900

    new vue application

---
+++ person/src/App.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import PostForm from "./components/PostForm.vue";
+</script>
+
+<template>
+  <div class="container">this is vue</div>
+  <PostForm />
+</template>
+
+<style>
+.container {
+  width: 100%;
+  height: auto;
+  display: flex;
+  justify-content: center;
+}
+</style>
---
+++ person/src/components/PostForm.vue
@@ -0,0 +1,45 @@
+<script setup lang="ts">
+import { reactive } from "vue";
+const postForm = () => {
+  alert(postData.name);
+};
+
+interface data {
+  name: string;
+  age: number;
+}
+
+const postData = reactive<data>({
+  name: "",
+  age: 0,
+});
+</script>
+
+<template>
+  <div class="container">
+    <form>
+      <div>
+        <label for="name">name: </label>
+        <input type="text" placeholder="name filed" v-model="postData.name" />
+      </div>
+      <div>
+        <label for="age">age: </label>
+        <input type="text" placeholder="age filed" v-model="postData.age" />
+      </div>
+      <button @click="postForm" class="postButton">register</button>
+    </form>
+  </div>
+</template>
+
+<style>
+.container {
+  width: 100%;
+  margin-top: 30px;
+}
+.postButton {
+  color: white;
+  background-color: greenyellow;
+  border-radius: 5px;
+  box-shadow: 20px;
+}
+</style>
