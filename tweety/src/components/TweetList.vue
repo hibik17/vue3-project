3f41c5ed4c6d308dc8e3a1a494b887fee6c194e3
hibik17 <hbtatsu@gmail.com>
2022-10-07 17:15:12 +0900

    application complete

---
+++ tweety/src/components/TweetList.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+// Propsの展開
+import { defineProps } from "vue";
+type valueArray = { id: number; description: string };
+type Props = { valueArray: valueArray[] };
+defineProps<Props>();
+
+// deleteTodoの展開
+const deleteTodo = (num: number) => {
+  console.log("this is deleteTodo");
+};
+</script>
+
+<template>
+  <div>
+    <div class="list_container">
+      <p v-if="valueArray.length === 0">no tweets have benn added</p>
+      <ul v-for="(value, index) in valueArray" :key="index">
+        <li>
+          <span>{{ value.description }}</span>
+        </li>
+        <button @click="deleteTodo(index)">Delete</button>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<style></style>
