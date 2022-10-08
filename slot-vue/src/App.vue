0be82b3dbae28c764d38abf6e61f0f8dac4c03a5
hibik17 <hbtatsu@gmail.com>
2022-10-08 16:51:23 +0900

    learn slot

---
+++ slot-vue/src/App.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+let msg = "this message is from vue script tag";
+</script>
+
+<template>
+  <div>{{ msg }}</div>
+  <div class="text-red-400 text-3xl">tailwind css</div>
+</template>
+
+<style></style>
---
+++ slot-vue/src/components/CardLIst.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import Card from "./Card.vue";
+</script>
+
+<template>
+  <div class="container">
+    <Card title="this is title" />
+  </div>
+</template>
+
+<style scoped></style>
